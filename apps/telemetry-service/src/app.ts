54b90a98c6a5eda5988afc27220edff5ed5b519f
Yahir Melo <yahiirmelo@gmail.com>
2025-10-01 15:53:36 -0600

    feat(config): integrate dotenv for environment variable management
    This commit introduces the 'dotenv' library to handle environment variables, decoupling sensitive configuration from the source code.
    
    Changes include:
    - Added 'dotenv' as a new project dependency.
    - Initialized dotenv at the application entry point (app.ts) to load variables from a .env file.
    - Updated tsconfig.json by adding "types": ["node"] to provide TypeScript with the necessary type definitions for the 'process.env' global object.
    
    This change significantly improves security by keeping secrets (like DB credentials and API keys) out of version control and simplifies configuration management across different environments (development, staging, production).

---
+++ apps/telemetry-service/src/app.ts
@@ -0,0 +1,24 @@
+
+
+import { findHeroById } from './services/hero.service.js'; // <-- SIN extensión
+
+
+
+const hero = findHeroById(1);
+console.log(hero?.name ?? 'no encontrado');
+
+// Importar y configurar dotenv
+import 'dotenv/config';
+
+// Ahora puedes acceder a tus variables de entorno a través de process.env
+const dbHost = process.env.DB_HOST;
+const dbUser = process.env.DB_USER;
+const apiKey = process.env.API_KEY_WEATHER;
+const port = process.env.PORT;
+
+console.log(`Conectando a la base de datos en: ${dbHost}\nUsuario: ${dbUser}`);
+console.log(`Usando la API Key: ${apiKey}\nPUERTO: ${port}`);
+
+
+
+// Aquí iría el resto de la lógica de tu aplicación...
