01f2f49be84f61e73ec6385fbf37a1b98b1ac46a
Yahir Melo <yahiirmelo@gmail.com>
2025-10-02 20:09:37 -0600

    feat(auth): implement register user controller and dto

--- apps/telemetry-service/src/controllers/auth_controller.ts
+++ apps/telemetry-service/src/controllers/auth_controller.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from "express"
+import { RegisterUserDto } from "../domain/dtos/auth/register.user.dto.js";
 
 
 
@@ -7,7 +8,12 @@ export class AuthController{
 constructor(){}
 
 registerUser = (req:Request,res:Response)=>{
-    res.json('registerUser');
+    const [err, registerUserDto]=RegisterUserDto.create(req.body);
+    if(err) return res.status(400).json(err)
+
+    
+    res.json(registerUserDto);
+
 }
 
 loginUser = (req:Request,res:Response)=>{
---
+++ apps/telemetry-service/src/domain/dtos/auth/register.user.dto.ts
@@ -0,0 +1,33 @@
+
+import { regularExps } from '../../../config/index.js'
+
+export class RegisterUserDto{
+
+ private constructor(
+
+    public name:string,
+    public email:string,
+    public password:string,
+
+  
+
+ ){}
+
+ static create(object:{[key:string]:any}):[string?,RegisterUserDto?]{
+
+  const{name,email,password}=object;
+  
+  if(!name) return ['Missing name'];
+  if(!email) return ['Missing email'];
+  if(!password) return ['Missing password'];
+  if(!regularExps.email.test(email)) return ['Invalid email'];
+  if(password.length<6) return ['Password must be at least 6 characters'];
+  
+  return [,new RegisterUserDto(name,email,password)];
+
+     
+  
+  
+ }
+
+}
