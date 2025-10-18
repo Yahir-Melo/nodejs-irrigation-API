f1bbf7100d095ca92ee7610942ec7d87a024a2b9
Yahir Melo <yahiirmelo@gmail.com>
2025-10-17 18:46:30 -0600

    feat(auth): Introduce refresh token logic

--- apps/auth-service/src/application/use-cases/login-user.usecase.ts
+++ apps/auth-service/src/application/use-cases/login-user.usecase.ts
@@ -6,9 +6,9 @@ import { CustomError } from '../../domain/errors/custom.error.js';
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 
-// NUEVO: Define un DTO de respuesta para el login.
 export interface LoginUserResponseDto {
-  token: string;
+  accessToken: string;
+  refreshToken: string;
   user: {
     id: string;
     name?: string;
@@ -16,10 +16,6 @@ export interface LoginUserResponseDto {
   };
 }
 
-/**
- * REFACTORIZADO:
- * El Caso de Uso ahora es responsable de la lógica de autenticación.
- */
 export class LoginUserUseCase {
 
   constructor(
@@ -28,33 +24,38 @@ export class LoginUserUseCase {
 
   async execute(dto: LoginUserDto): Promise<LoginUserResponseDto> {
     
-    // 1. Buscar al usuario por su email usando el repositorio limpio.
     const user = await this.userRepository.findByEmail(dto.email);
     if (!user) { 
-      throw CustomError.unauthorized('Credenciales inválidas.'); // Error genérico por seguridad
+      throw CustomError.unauthorized('Credenciales inválidas.');
     }
 
-    // 2. Verificar si el correo está verificado directamente en la entidad.
     if (!user.emailVerified) { 
       throw CustomError.unauthorized('Correo electrónico no ha sido validado.');
     }
 
-    // 3. Comparar la contraseña aquí, en el caso de uso.
     const isPasswordCorrect = await bcrypt.compare(dto.password, user.passwordHash);
     if (!isPasswordCorrect) {
       throw CustomError.unauthorized('Credenciales inválidas.'); 
     }
 
-    // 4. Generar el token de sesión JWT aquí.
-    const token = jwt.sign(
+    const accessToken = jwt.sign(
       { id: user.id, email: user.email, role: user.role },
-      process.env.JWT_SECRET || "mi_clave_super_segura",
+      "access_secret",
+      { expiresIn: "15m" }
+    );
+
+    const refreshToken = jwt.sign(
+      { id: user.id, email: user.email, role: user.role },
+      "refresh_secret_super_segura",
       { expiresIn: "7d" }
     );
 
-    // 5. Mapear la entidad a un DTO de respuesta seguro.
+    user.refreshTokens.push(refreshToken);
+    await this.userRepository.save(user);
+
     return {
-      token,
+      accessToken,
+      refreshToken,
       user: {
         id: user.id! || '',
         name: user.name || '',
