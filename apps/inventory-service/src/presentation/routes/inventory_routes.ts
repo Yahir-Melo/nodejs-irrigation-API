ce4c342f0e934b2fea30effed33f6bc6b4598833
Yahir Melo <yahiirmelo@gmail.com>
2025-10-27 20:54:28 -0600

    feat(inventory): Agregadas rutas CRUD para productos

--- apps/inventory-service/src/presentation/routes/inventory_routes.ts
+++ apps/inventory-service/src/presentation/routes/inventory_routes.ts
@@ -2,19 +2,28 @@ import { Router } from "express";
 import { InventoryController } from "../controllers/inventory_controller.js";
 import { AuthMiddleware } from "../middlewares/auth.middleware.js";
 
-export class InventoryRoutes{
+export class InventoryRoutes {
 
-   static get routes(): Router{
+   static get routes(): Router {
  
-    const router= Router();
+    const router = Router();
+    const controller = new InventoryController();
 
-     const controller=new InventoryController()
-     router.post('/profile',AuthMiddleware.validateJWT,controller.profile,);
+    // ✅ CREAR un nuevo producto
+    router.post('/products', AuthMiddleware.validateJWT, controller.createProduct);
+
+    // ✅ LEER todos los productos
+    router.get('/products', AuthMiddleware.validateJWT, controller.getProducts);
+
+    // ✅ LEER un solo producto por su ID
+    router.get('/products/:id', AuthMiddleware.validateJWT, controller.getProductById);
+
+    // ✅ ACTUALIZAR un producto por su ID
+    router.put('/products/:id', AuthMiddleware.validateJWT, controller.updateProduct);
+
+    // ✅ BORRAR un producto por su ID
+    router.delete('/products/:id', AuthMiddleware.validateJWT, controller.deleteProduct);
 
      return router;
    }  
-
-
-  
-
 }
